07d420867edae44375353b4169c7ef7fffb63fef
David Urvoy <david.urvoy.pro@gmail.com>
2021-11-11 19:35:21 +0000

    mov

--- main/01-basics/01-variables.js
+++ main/01-basics/01-variables.js
@@ -1,8 +1,23 @@
-export function swap(a, b) {
-    console.log("hello, world!")
-    return 'hello world!'
+/** First function : Hello world ! */
+export function helloWorld() {
+    return 'Hello, world!'
 }
 
-swap()
+/** 
+ * Write a function that returns the sum of the two numbers passed as arguments.
+ */
+export function sum(a, b) {
+    return a + b
+}
 
-/**  */
+/**
+ * Write a function that converts Celcius degrees into Fahrenheit.
+ * 
+ * °F = °C * 9/5 + 32
+ * 
+ * @returns the temperature converted to Fahrenheit
+ */
+export function celciusToFahrenheit(celciusTemperature) {
+    return celciusTemperature * (9/5) + 32
+}
+
--- test/01-basics/01-variables.test.js
+++ test/01-basics/01-variables.test.js
@@ -1,5 +1,18 @@
-import {swap} from '../../main/01-basics/01-variables.js'
+import { celciusToFahrenheit, helloWorld, sum } from '../../main/01-basics/01-variables.js'
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(swap()).toEqual('hello world!');
-  });
+test('should return "Hello, world!"', () => {
+  expect(helloWorld()).toEqual('Hello, world!')
+});
+
+describe('should convert Celcius degrees to Fahrenheit', () => {
+  test("32 °C = 89.6 °F", () => expect(celciusToFahrenheit(32)).toEqual(89.6))
+  test("1 °C = 33.8 °F", () => expect(celciusToFahrenheit(1)).toEqual(33.8))
+  test("-10 °C = 14 °F", () => expect(celciusToFahrenheit(-10)).toEqual(14))
+})
+
+describe('should apply sum of two numbers', () => {
+  test('0 + 0 = 0', () => expect(sum(0, 0)).toEqual(0))
+  test('1 + 1 = 2', () => expect(sum(1, 1)).toEqual(2))
+  test('5 + 8 = 13', () => expect(sum(5, 8)).toEqual(13))
+  test('-4 + 2 = -2', () => expect(sum(-4, 2)).toEqual(-2))
+})
