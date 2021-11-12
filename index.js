a128f5393fb7fa82584fd87bcca271cd72de65e5
David Urvoy <david.urvoy.pro@gmail.com>
2021-11-12 16:44:43 +0000

    CLR

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import {swap} from './main/01-basics/01-variables.js'
-import {} from './test/01-basics/01-variables.test.js'
+import readline from 'readline'
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'INPUT> '
+});
 
-swap()
+async function read() {
+    rl.prompt();
+    let a;
+    rl.on('line', (line) => {
+        a = line.trim()
+        // process.exit(0);
+    })
+    console.log(`input: ${a}`)
+}
+
+let a = await read()
+await console.log(`I received: ${a}`)
