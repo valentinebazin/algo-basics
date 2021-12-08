d532971d6f2a2e8f27723f613871dbb941680b99
David Urvoy <david.urvoy.pro@gmail.com>
2021-12-08 16:20:04 +0000

    trading

---
+++ main/03-advanced/trading.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function that given an array of stock prices, find the minimum buy price and the maximum sell price that produce the greatest profit.
+ * Return the result as an array containing the two prices.
+ * 
+ * The array will contain numbers that represents the prices of stocks that are varying over time, and the objective is to maximize your profits.
+ * BUY HIGH, SELL LOW.
+ * 
+ * E.g : [2, 1, 5, 3, 4]  :   the best would be to buy when the stock when it costs 1$, and sell it for 5$, so [1, 5]
+ */
+export function maximizeProfits(prices) {
+
+}
